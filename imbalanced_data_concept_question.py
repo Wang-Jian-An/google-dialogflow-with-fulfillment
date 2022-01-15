3d2fc74790860f53a95bddd49a9e7d5b7263aa58
Jian-An Wang <facebook0983481181@gmail.com>
2022-01-15 19:52:01 +0800

    correct error in imbalanced_data_concept_question.py

--- imbalanced_data_concept_question.py
+++ imbalanced_data_concept_question.py
@@ -5,5 +5,5 @@ def generate_answer_text():
 
     answer_text = "使用者你好，在網路上有專家為您解答您的問題囉，\n在此附上連結，並建議您參考以下資訊：\n\n"
     for one_row in range(qa_data.shape[0]):
-        answer_text += f"主題：{qa_data.loc[one_row, "QA主題"]}\n參考連結：{qa_data.loc[one_row, "QA連結"]}"
+        answer_text += "主題：" + qa_data.loc[one_row, "QA主題"] + "\n參考連結：" + qa_data.loc[one_row, "QA連結"]
     return answer_text
