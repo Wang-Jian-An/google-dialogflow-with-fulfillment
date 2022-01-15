359f89a61ef41033326aad80994febb6d17468c6
Jian-An Wang <facebook0983481181@gmail.com>
2022-01-15 20:28:21 +0800

    change input data

--- imbalanced_data_concept_question.py
+++ imbalanced_data_concept_question.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def generate_answer_text():
-    # qa_data = pd.read_excel("Cupoy_QA問答集.xlsx", sheet_name = "不平衡資料")
+    qa_data = pd.read_csv("Cupoy_QA問答集_不平衡資料.csv")
 
     answer_text = "使用者你好，在網路上有專家為您解答您的問題囉，在此附上連結，並建議您參考以下資訊："
     # for one_row in range(qa_data.shape[0]):
