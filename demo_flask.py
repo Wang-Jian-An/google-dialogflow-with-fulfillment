12f838c69812d1cc8df4bdb3b3b3b2213b1f9b16
Jian-An Wang <facebook0983481181@gmail.com>
2022-01-16 14:03:53 +0800

    test

--- demo_flask.py
+++ demo_flask.py
@@ -12,15 +12,16 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     fulfillmentText = ""
     query_result = req.get("queryResult")
-    query_text_store.store_text(query_result.get("queryText"))
+    text = query_result.get("queryText")
+    query_text_store.store_text(text)
     if query_result.get("action") == "question_about_imbalanced_data_handle":
         fulfillmentText = question_about_imbalanced_data_handle.generate_answer_text()
     elif query_result.get("action") == "question_about_imbalanced_data_concept":
         fulfillmentText = question_about_imbalanced_data_concept.generate_answer_text()
     elif query_result.get("action") == "precision_equation":
         fulfillmentText = "Precision相關問題"
-        with open("query_text_store_precision.txt", "r") as f:
-            fulfillmentText += " ".join(f.readlines)
+        # with open("query_text_store_precision.txt", "r") as f:
+        #     fulfillmentText += " ".join(f.readlines)
     elif query_result.get("action") == "question_about_the_concept_of_transfer_learning":
         fulfillmentText = question_about_the_concept_of_transfer_learning.generate_answer_text()
     return {
--- query_text_store.py
+++ query_text_store.py
@@ -1,5 +1,7 @@
 
 def store_text(text):
     with open("query_text_store_precision_equation.txt", "a") as f:
-        f.write(text)
+        f.write(text + "\n")
     return
+
+store_text("precision")
