3b6ded3668762aff63964c116b3dcb930dc3db46
Jian-An Wang <facebook0983481181@gmail.com>
2022-01-16 14:05:47 +0800

    test

--- demo_flask.py
+++ demo_flask.py
@@ -20,8 +20,8 @@ def webhook():
         fulfillmentText = question_about_imbalanced_data_concept.generate_answer_text()
     elif query_result.get("action") == "precision_equation":
         fulfillmentText = "Precision相關問題"
-        # with open("query_text_store_precision.txt", "r") as f:
-        #     fulfillmentText += " ".join(f.readlines)
+        with open("query_text_store_precision.txt", "r") as f:
+            fulfillmentText += " ".join(f.readlines())
     elif query_result.get("action") == "question_about_the_concept_of_transfer_learning":
         fulfillmentText = question_about_the_concept_of_transfer_learning.generate_answer_text()
     return {
